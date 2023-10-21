95f36bf48660accc5c177d95b05c9c44eafaab6a
VictorSadykov <svn1409@gmail.com>
2023-10-21 17:59:38 +0800

    update

--- 18-3-practica.py
+++ 18-3-practica.py
@@ -9,53 +9,10 @@
 
 import os
 
-from src.utils import get_youtube_data, create_database, save_data_to_database
+from utils import get_youtube_data, create_database, save_data_to_database
 from config import config
 
 
-def create_database(database_name: str, params: dict):
-    """Создание базы данных и таблиц для сохранения данных о каналах и видео."""
-
-    conn = psycopg2.connect(dbname='postgres', **params)
-    conn.autocommit = True
-
-    with conn.cursor() as cur:
-        # удаление базы если она есть
-        cur.execute(f"DROP DATABASE IF EXISTS {database_name}")
-
-        cur.execute(f"CREATE DATABASE {database_name}")
-
-    conn.close()
-
-    conn = psycopg2.connect(dbname=database_name, **params)
-
-    with conn.cursor() as cur:
-        cur.execute("""
-            CREATE TABLE channels (
-                channel_id SERIAL PRIMARY KEY,
-                title VARCHAR(255) NOT NULL,
-                views INTEGER,
-                subscribers INTEGER,
-                videos INTEGER,
-                channel_url TEXT
-            )
-        """)
-
-    conn.commit()
-
-    with conn.cursor() as cur:
-        cur.execute("""
-            CREATE TABLE videos (
-                video_id SERIAL PRIMARY KEY,
-                channel_id INT REFERENCES channels(channel_id),
-                title VARCHAR NOT NULL,
-                publish_date DATE,
-                video_url TEXT
-            )
-        """)
-
-    conn.commit()
-    conn.close()
 
 def main():
     api_key = os.getenv('YT_API_KEY')
